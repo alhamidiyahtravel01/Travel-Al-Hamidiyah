7611751258df834dd850ee2ea39c955fbb9cf298
alhamidiyahtravel01 <travelalhamidiyah@gmail.com>
2025-06-19 18:47:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("formPendaftaran").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Pendaftaran Anda telah dikirim. Kami akan menghubungi Anda secepatnya.");
+});
